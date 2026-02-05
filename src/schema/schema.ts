3efbe335e7a53120667c366a9d2f8d6faca6a932
Alan Reis <alanreisanjo@gmail.com>
2026-02-05 18:05:40 -0300

    Adição de Schema.map

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,20 +1,28 @@
-import type { TSchema } from "@sinclair/typebox";
+import type { TSchema, Static } from "@sinclair/typebox";
+import { Value } from "@sinclair/typebox/value";
 import { TypeCompiler, type TypeCheck } from "@sinclair/typebox/compiler";
 import "./formats";
 
-export class Schema {
+export class Schema<SchemaType extends TSchema> {
 	private compiledSchema: TypeCheck<TSchema>;
 
-	constructor(private schema: TSchema) {
+	constructor(private schema: SchemaType) {
 		this.compiledSchema = TypeCompiler.Compile(this.schema);
 	}
 
-	static make(schema: TSchema): Schema {
+	static make<SchemaType extends TSchema>(
+		schema: SchemaType,
+	): Schema<SchemaType> {
 		return new Schema(schema);
 	}
 
 	public match(content: unknown): boolean {
 		return this.compiledSchema.Check(content);
+	}
+
+	public map(content: unknown): Static<SchemaType> {
+		console.log(Value.Cast(this.schema, Value.Convert(this.schema, content)));
+		return Value.Cast(this.schema, Value.Convert(this.schema, content));
 	}
 
 	public toString(): string {
